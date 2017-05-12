b541e2f106401736932618ac627d92c8fff0c774
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-05-12 15:09:24 +0800

    added string, println

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Forjery is a Forth dialect running on JVM.
 
 ```forth
+." Hello, world!" println
+\ Pops the string and print it with a newline.
 1 2 3 * 4 5 / - + .s
 \ Equivalent to 1 + 2 * 3 - 4 / 5.
 ```
--- src/jvm/forjery/VM.java
+++ src/jvm/forjery/VM.java
@@ -3,20 +3,30 @@ import java.nio.charset.*;
 import java.util.*;
 
 enum TkType {
-    Num, Pop, Sstk, Add, Sub, Mul, Div
+    /* Number. */
+    Num,
+    /* String. */
+    Sos, Str,
+    /* Operations. */
+    Pop,
+    Sstk,
+    Add, Sub, Mul, Div,
+    Println
 }
 
 class Token {
-    private TkType ty;
-    private int val;
+    public TkType ty;
+    public Token(TkType ty) { this.ty = ty; }
+}
 
-    public Token(TkType ty, int val) {
-        this.ty = ty;
-        this.val = val;
-    }
+class TkNum extends Token {
+    public int val;
+    public TkNum(TkType ty, int val) { super(ty); this.val = val; }
+}
 
-    public TkType getType() { return this.ty; }
-    public int getValue() { return this.val; }
+class TkStr extends Token {
+    public String val;
+    public TkStr(TkType ty, String val) { super(ty); this.val = val; }
 }
 
 class Lexer {
@@ -32,45 +42,52 @@ class Lexer {
 
     public LinkedList<Token> scan() throws IOException {
         int t,              // temp
+            val,            // current token value
             row, col;       // line and column
         String buf;         // identifier buffer
         char p;             // source current position
-        TkType ty; int val; // current token
+        Token tk;           // last token
+        TkType ty;          // current token type
 
-        buf = ""; ty = TkType.Num; val = 0;
+        buf = ""; ty = TkType.Num; val = 0; tk = null;
         for (row = col = 1; (t = r.read()) > -1; ++col) {
             p = (char) t;
 
-            // Re-initialize for every character.
-            ty = TkType.Num; val = 0;
-                ty = TkType.Num;
-                val = Character.getNumericValue(p);
-            if (p > 32 && p < 127) {    // whitespace is 31
-                buf += p;
-                continue;
-            } else if (p == ' ' || p == '\n') {
-                if (buf.equals("")) continue;
+            if (p > 31 && p < 127 || p == '\n') {
+                // Sos: Start of string, means a string literal.
+                if (tk != null && tk.ty == TkType.Sos) {
+                    if (p != '"') { buf += p; continue; }
+                    tk = new TkStr(TkType.Str, buf);
+                    buf = ""; ty = TkType.Num;
+                } else if (p == ' ' || p == '\n') {
+                    if (buf.equals("")) continue;
 
-                if ( (ty = rw.get(buf)) == null) {
-                    try {
-                        val = Integer.parseInt(buf);
-                        ty = TkType.Num;
-                    } catch (NumberFormatException e) {
-                        System.err.println(row + ":" + col +
-                                " Invalid identifier \"" + buf + "\"");
-                        System.exit(1);
-                    }
-                }
+                    if ( (ty = rw.get(buf)) == null) {
+                        try {
+                            // Number literal.
+                            val = Integer.parseInt(buf);
+                            ty = TkType.Num;
+                            tk = new TkNum(ty, val);
+                        } catch (NumberFormatException e) {
+                            System.err.println(row + ":" + col +
+                                    " Invalid identifier \"" + buf + "\"");
+                            System.exit(1);
+                        }
+                    } else tk = new Token(ty);
+                } else { buf += p; continue; }
 
                 if (p == '\n') { ++row; col = 1; }
                 buf = "";
+
+                // Don't put Sos into emitted code.
+                if (tk.ty == TkType.Sos) continue;
             } else {
                 System.err.println(row + ":" + col +
-                        " Invalid token \"" + p + "\"");
+                        " Invalid character \"" + p + "\"");
                 System.exit(1);
             }
 
-            e.push(new Token(ty, val));
+            e.push(tk);
         }
 
         return e;
@@ -80,10 +97,8 @@ class Lexer {
 public class VM {
     private static String usage = "Usage: forjery file";
     private static HashMap<String, TkType> rw;  // reserved words
-
     private static LinkedList<Token> e;         // emitted code
-    // TODO: Heterogeneous stack, not only for Integers.
-    private static LinkedList<Integer> s;       // stack
+    private static LinkedList<Token> s;         // stack
 
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
@@ -100,12 +115,14 @@ public class VM {
 
         // Initialize reserved words table.
         rw = new HashMap<String, TkType>();
+        rw.put(".\"", TkType.Sos);
         rw.put(".", TkType.Pop);
         rw.put(".s", TkType.Sstk);
         rw.put("+", TkType.Add);
         rw.put("-", TkType.Sub);
         rw.put("*", TkType.Mul);
         rw.put("/", TkType.Div);
+        rw.put("println", TkType.Println);
 
         // Start lexical analysis.
         Lexer lexer = new Lexer(br, rw);
@@ -114,23 +131,55 @@ public class VM {
         // Run.
         int a, b;                       // temps
         Token tk;                       // current token
-        s = new LinkedList<Integer>();  // init stack
+        s = new LinkedList<Token>();  // init stack
 
         while (e.size() != 0) {
             tk = e.removeLast();
-            switch (tk.getType()) {
-                case Num: s.push(tk.getValue()); break;
+            switch (tk.ty) {
+                case Num: s.push(new TkNum(TkType.Num, ((TkNum)tk).val)); break;
+                case Str: s.push(new TkStr(TkType.Str, ((TkStr)tk).val)); break;
                 case Pop: s.pop(); break;
                 case Sstk:
-                    System.out.println("HEAD" + Arrays.toString(s.toArray()));
+                    System.out.print("<" + s.size() + "> ");
+                    for (int i = 0; i < s.size(); ++i) {
+                        if (s.get(i) instanceof TkNum) {
+                            System.out.print(((TkNum) s.get(i)).val + " ");
+                        } else if (s.get(i) instanceof TkStr) {
+                            System.out.print(
+                                "\"" + ((TkStr) s.get(i)).val + "\" "
+                            );
+                        } else {
+                            System.out.print(tk.ty + " ");
+                        }
+                    }
+                    System.out.println("ok");
                     break;
-                case Add: s.push(s.pop() + s.pop()); break;
-                case Mul: s.push(s.pop() * s.pop()); break;
-                case Sub: a = s.pop(); b = s.pop(); s.push(b - a); break;
-                case Div: a = s.pop(); b = s.pop(); s.push(b / a); break;
+                case Add:
+                    a = ((TkNum) s.pop()).val; b = ((TkNum) s.pop()).val;
+                    s.push(new TkNum(TkType.Num, b + a));
+                    break;
+                case Mul:
+                    a = ((TkNum) s.pop()).val; b = ((TkNum) s.pop()).val;
+                    s.push(new TkNum(TkType.Num, b * a));
+                    break;
+                case Sub:
+                    a = ((TkNum) s.pop()).val; b = ((TkNum) s.pop()).val;
+                    s.push(new TkNum(TkType.Num, b - a));
+                    break;
+                case Div:
+                    a = ((TkNum) s.pop()).val; b = ((TkNum) s.pop()).val;
+                    s.push(new TkNum(TkType.Num, b / a));
+                    break;
+                case Println:
+                    tk = s.pop();
+                    if (tk instanceof TkNum) {
+                        System.out.println(((TkNum) tk).val);
+                    } else if (tk instanceof TkStr) {
+                        System.out.println(((TkStr) tk).val);
+                    } else System.out.println(tk.ty);
+                    break;
                 default:
-                    System.err.println("Invalid token tk=" + tk.getType() +
-                            " val=" + tk.getValue());
+                    System.err.println("Invalid token tk=" + tk.ty);
             }
         }
     }
